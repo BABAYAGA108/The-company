80544b263985d16b8d4e0e842dc273bfda75de1b
kalu honour <honourkalu216@gmail.com>
2025-07-04 11:37:44 +0100

    home,about,service,contact, added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,14 @@ const Navbar = () => {
   return (
     <nav>
       <div>
-        <div>
-          <a href="/">Logo</a>
+        <div className="flex items-center gap-2">
+          <a href="#"><img src="" alt="logo" /></a>
+          <span className="text-2xl font-bold ">
+          The Company
+          </span>
         </div>
         <div>
-          <form className="group relative" role="search" aria-label="Site Search">
+          <form className="group relative" role="search" aria-label="Site Search ">
             <label htmlFor="navbar-search" className="sr-only">
               Search
             </label>
@@ -17,11 +20,13 @@ const Navbar = () => {
               className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-orange-400"
             />
           </form>
-          <ul className="">
-            <li> Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
+         
+        </div>
+        <div className="flex items-center gap-4">
+          <a href="#" className="text-gray-700 hover:text-orange-500">Home</a>
+          <a href="#" className="text-gray-700 hover:text-orange-500">About</a>
+          <a href="#" className="text-gray-700 hover:text-orange-500">Services</a>
+          <a href="#" className="text-gray-700 hover:text-orange-500">Contact</a>
         </div>
       </div>
     </nav>

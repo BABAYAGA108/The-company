6bcfef71a1df4eefe3f374d681434c3ca67b6b51
kalu honour <honourkalu216@gmail.com>
2025-07-07 13:26:26 +0100

    hero sectuon

---
+++ src/components/hero page/hero.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+
+// ...other imports
+
+const Hero = () => {
+  return (
+	<div>
+	  <Navbar />
+	  {/* ...rest of your hero component */}
+	</div>
+  );
+};
+
+export default Hero;

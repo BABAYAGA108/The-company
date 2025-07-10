2629a0eff7da348ae8476a6634be9558dfbfc66c
kalu honour <honourkalu216@gmail.com>
2025-07-10 13:10:52 +0100

    social media icons

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -15,9 +15,11 @@ const imagelist={
 }
 const Hero =()=>{
   return (
-    <section className="flex-1 mr:28 md:text-left mt:0 relative"
-     data-aos="fade-up"
-     data-aos-duration="1000">
+    <section
+      className="flex-1 mr:28 md:text-left mt:0 relative"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+    >
       <div>
         <div>
           <p className="text-center text-2xl md:text-3xl font-bold mb-4">
@@ -26,9 +28,59 @@ const Hero =()=>{
         </div>
         <div>
           <h1 className="text-x1 sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-4">
-            frontend developer    
+            frontend developer
           </h1>
 
+          <p className="text-center text-lg md:text-xl mb-6">
+            <a href="">
+              <img
+                src={image1}
+                alt="Image 1"
+                className="w-40 h-48 h object-cover mx-auto mb-4"
+              />
+            </a>
+            I am a frontend developer with a passion for creating beautiful and
+            functional user interfaces. I have experience in HTML, CSS, and
+            JavaScript, react, and and I am always eager to learn new
+            technologies and improve my skills.
+          </p>
+          <div className="flex justify-center gap-4 mb-6">
+            <a
+              href="https://x.com/kalu_honour"
+              className="text-2xl text-blue-500 hover:text-blue-700 transition-colors"
+              aria-label="Twitter"
+            >
+              <FaXTwitter />
+            </a>
+            <a
+              href="https://instagram.com/yourprofile"
+              className="text-2xl text-pink-500 hover:text-pink-700 transition-colors"
+              aria-label="Instagram"
+            >
+              <IoLogoInstagram />
+            </a>
+            <a
+              href="https://twitter.com/yourprofile"
+              className="text-2xl text-blue-500 hover:text-blue-700 transition-colors"
+              aria-label="Twitter"
+            >
+              <MdAttachEmail />
+            </a>
+            <a
+              href="https://twitter.com/yourprofile"
+              className="text-2xl text-blue-500 hover:text-blue-700 transition-colors"
+              aria-label="Twitter"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://twitter.com/yourprofile"
+              className="text-2xl text-blue-500 hover:text-blue-700 transition-colors"
+              aria-label="Twitter"
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
         </div>
       </div>
     </section>

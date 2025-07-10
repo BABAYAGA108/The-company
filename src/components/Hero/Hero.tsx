0d8eee9a2887e46fc03fe23c502cc985e1da279e
kalu honour <honourkalu216@gmail.com>
2025-07-10 11:35:35 +0100

    working hero page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,8 @@
-
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoInstagram } from "react-icons/io";
+import { MdAttachEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa6";
 import { Children } from "react";
 import image1 from "../../assets/images/hello and welcome.jpeg";
 import image2 from "../../assets/images/number 1.jpeg";
@@ -11,15 +15,23 @@ const imagelist={
 }
 const Hero =()=>{
   return (
-    <div>
-    
+    <section className="flex-1 mr:28 md:text-left mt:0 relative"
+     data-aos="fade-up"
+     data-aos-duration="1000">
       <div>
-        <p>
-          <h1 className="flex justify-center">about myself</h1>
-    
-        </p>
+        <div>
+          <p className="text-center text-2xl md:text-3xl font-bold mb-4">
+            <h1 className="">about myself</h1>
+          </p>
+        </div>
+        <div>
+          <h1 className="text-x1 sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-4">
+            frontend developer    
+          </h1>
+
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
